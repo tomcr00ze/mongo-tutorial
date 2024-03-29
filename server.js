b3174e31eda72208f2da1d4cf7dcb6d52d82d9b4
tomcr00ze <tomcroose101@gmail.com>
2024-03-29 11:19:29 +0530

    added mongodb connection code

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 // First, we imported the dependencies which we need i.e Express and Body Parser.
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// This code snippet sets the default promise implementation for Mongoose 
+// to be the same as the global Promise implementation provided by JavaScript.
+// Node.js, the global Promise object is used for asynchronous operations that return a value or throw an error.
+// By setting mongoose.Promise to global.Promise, we're essentially instructing Mongoose to use the 
+// same promise implementation as the one provided by the JavaScript environment in which it is running.
+mongoose.Promise = global.Promise
+
+// This code establishes a connection to a MongoDB database using Mongoose
+mongoose.connect(YOUR_MONGODB_URL, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Error...', err);
+    process.exit();
+});
 
 const app = express()
 
